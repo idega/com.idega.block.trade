f57143b8699b1f1b9d731fbb8039d9dfe9caf344
Grimur Jonsson <gimmi@idega.com>
2006-03-02 17:12:57 +0000

    removed printing out the sql for getting price

--- src/java/com/idega/block/trade/stockroom/business/StockroomBusinessBean.java
+++ src/java/com/idega/block/trade/stockroom/business/StockroomBusinessBean.java
@@ -232,7 +232,6 @@ public class StockroomBusinessBean extends IBOServiceBean implements StockroomBu
           		return price.getPrice();
           	}
           }else{
-            System.err.println(buffer.toString());
             throw new ProductPriceException("No Price Was Found");
           }
         }else if(cat.getType().equals(com.idega.block.trade.stockroom.data.PriceCategoryBMPBean.PRICETYPE_DISCOUNT)){
