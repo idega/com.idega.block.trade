c928d525ad6a1e2d8230dc85b0779143f3ba25e3
Grimur Jonsson <gimmi@idega.com>
2006-07-01 15:06:01 +0000

    New ROLE for SupplierManager, Currency Setter

--- src/java/com/idega/block/trade/stockroom/business/TradeConstants.java
+++ src/java/com/idega/block/trade/stockroom/business/TradeConstants.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TradeConstants.java,v 1.6 2006/02/28 13:44:29 gimmi Exp $
+ * $Id: TradeConstants.java,v 1.7 2006/07/01 15:06:01 gimmi Exp $
  * Created on 9.2.2005
  *
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -12,21 +12,19 @@ package com.idega.block.trade.stockroom.business;
 
 /**
  * 
- *  Last modified: $Date: 2006/02/28 13:44:29 $ by $Author: gimmi $
+ *  Last modified: $Date: 2006/07/01 15:06:01 $ by $Author: gimmi $
  * 
  * @author <a href="mailto:gimmi@idega.com">gimmi</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 public class TradeConstants {
 	
-	public static String SUPPLIER_MANAGER_ROLE_KEY = "supp_man_edit_role";
-	public static String ROLE_SUPPLIER_MANAGER_BOOKING_STAFF = "supp_man_book_staff";
-	public static String ROLE_SUPPLIER_MANAGER_TRAVEL_AGENT_STAFF = "supp_man_ta_staff";
-	public static String ROLE_SUPPLIER_MANAGER_PARTNER_STAFF = "supp_man_partner_staff";
-//	public static String ROLE_CREATE_EDIT_PRODUCTS = "supp_man_role_create_edit_products";
-//	public static String ROLE_EDIT_SUPPLIES = "supp_man_role_edit_supplies";
-//	public static String ROLE_TRAVEL_AGENCY = "supp_man_role_agency";
-	public static String ROLE_BOOKING_BASKET = "supp_man_role_book_basket";
-	public static String ROLE_SUPPLIER_MANAGER_CASHIER_STAFF = "supp_man_cash_staff";
-	public static String ROLE_ADVANCED_VOUCHER = "supp_man_adv_voucher";
+	public static final String SUPPLIER_MANAGER_ROLE_KEY = "supp_man_edit_role";
+	public static final String ROLE_SUPPLIER_MANAGER_BOOKING_STAFF = "supp_man_book_staff";
+	public static final String ROLE_SUPPLIER_MANAGER_TRAVEL_AGENT_STAFF = "supp_man_ta_staff";
+	public static final String ROLE_SUPPLIER_MANAGER_PARTNER_STAFF = "supp_man_partner_staff";
+	public static final String ROLE_BOOKING_BASKET = "supp_man_role_book_basket";
+	public static final String ROLE_SUPPLIER_MANAGER_CASHIER_STAFF = "supp_man_cash_staff";
+	public static final String ROLE_ADVANCED_VOUCHER = "supp_man_adv_voucher";
+	public static final String ROLE_CURRENCY_SETTER = "supp_man_currency_setter";
 }
