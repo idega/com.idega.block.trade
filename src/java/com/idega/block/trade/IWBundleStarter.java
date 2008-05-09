b4c24f778889f64fc7e88899ff807e50ca49a861
Grimur Jonsson <gimmi@idega.com>
2008-05-09 05:29:28 +0000

    moved to a default location

---
+++ src/java/com/idega/block/trade/IWBundleStarter.java
@@ -0,0 +1,14 @@
+package com.idega.block.trade;
+
+import java.awt.event.ActionListener;
+
+import com.idega.block.trade.business.TradeBundleStarter;
+import com.idega.idegaweb.IWBundleStartable;
+
+public class IWBundleStarter extends TradeBundleStarter implements IWBundleStartable, ActionListener {
+	
+	public IWBundleStarter() {
+		super();
+	}
+
+}
