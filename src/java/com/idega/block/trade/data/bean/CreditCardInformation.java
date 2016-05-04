e1a5e4fbdb0e6797a6b51baf9a4ea3db3610f424
Arūnas Proscevičius <arunas.proscevicius@gmail.com>
2016-05-05 01:24:43 +0300

    query fix

--- src/java/com/idega/block/trade/data/bean/CreditCardInformation.java
+++ src/java/com/idega/block/trade/data/bean/CreditCardInformation.java
@@ -5,8 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.idega.user.data.bean.Group;
@@ -14,10 +16,15 @@ import com.idega.user.data.bean.Group;
 @Entity
 @Table(name = CreditCardInformation.ENTITY_NAME)
 @NamedQueries({
-	@NamedQuery(name = CreditCardInformation.findBySupplierManager, query = "select cci from CreditCardInformation cci left join Group as grp where grp.groupID = :" + CreditCardInformation.supplierProp ),
-	@NamedQuery(name = CreditCardInformation.findByMerchant, query = "select cci from CreditCardInformation cci where cci." + CreditCardInformation.mpkProp + " = :" + CreditCardInformation.mpkProp + " and cci." + CreditCardInformation.typeProp + " = :" + CreditCardInformation.typeProp),
-	@NamedQuery(name = CreditCardInformation.findByPrimaryKey, query = "select cci from CreditCardInformation cci where cci." + CreditCardInformation.idProp + " = :" + CreditCardInformation.idProp )
-})
+
+		@NamedQuery(name = CreditCardInformation.findBySupplierManager, query = "select cci from CreditCardInformation cci left join cci.supplierManager where cci.supplierManager.groupID = :"
+				+ CreditCardInformation.supplierProp),
+		@NamedQuery(name = CreditCardInformation.findByMerchant, query = "select cci from CreditCardInformation cci where cci."
+				+ CreditCardInformation.mpkProp + " = :" + CreditCardInformation.mpkProp + " and cci."
+				+ CreditCardInformation.typeProp + " = :" + CreditCardInformation.typeProp),
+		@NamedQuery(name = CreditCardInformation.findByPrimaryKey, query = "select cci from CreditCardInformation cci where cci."
+				+ CreditCardInformation.idProp + " = :" + CreditCardInformation.idProp) })
+
 public class CreditCardInformation {
 	public static final String ENTITY_NAME = "CC_INFORMATION";
 	private static final String COLUMN_TYPE = "CC_TYPE";
@@ -27,25 +34,26 @@ public class CreditCardInformation {
 	public static final String findBySupplierManager = "CreditCardInformation.findBySupplierManager";
 	public static final String findByMerchant = "CreditCardInformation.findByMerchant";
 	public static final String findByPrimaryKey = "CreditCardInformation.findByPrimaryKey";
-	
+
 	public static final String idProp = "id";
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = CreditCardInformation.ENTITY_NAME+"_ID")
+	@Column(name = CreditCardInformation.ENTITY_NAME + "_ID")
 	private Integer id;
-	
+
 	public static final String typeProp = "type";
 	@Column(name = COLUMN_TYPE)
 	private String type;
-	
+
 	public static final String mpkProp = "merchantPK";
 	@Column(name = COLUMN_MERCHANT_PK)
 	private String merchantPK;
 
 	public static final String supplierProp = "supplierManager";
-	@Column(name = COLUMN_SUPPLIER_MANAGER_ID)
+	@OneToOne
+	@JoinColumn(name = COLUMN_SUPPLIER_MANAGER_ID)
 	private Group supplierManager;
-	
+
 	public String getType() {
 		return type;
 	}
