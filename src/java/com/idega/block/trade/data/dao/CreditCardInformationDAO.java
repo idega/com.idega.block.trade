005e773d4c191f1fc25ec05f2de58494b0b0aa65
Valdas Žemaitis <valdas@idega.com>
2020-08-13 13:44:07 +0300

    Extending GenericDao

--- src/java/com/idega/block/trade/data/dao/CreditCardInformationDAO.java
+++ src/java/com/idega/block/trade/data/dao/CreditCardInformationDAO.java
@@ -2,16 +2,18 @@ package com.idega.block.trade.data.dao;
 
 import java.util.List;
 
-import com.idega.user.data.bean.Group;
 import com.idega.block.trade.data.bean.CreditCardInformation;
+import com.idega.core.persistence.GenericDao;
+import com.idega.user.data.bean.Group;
 
-public interface CreditCardInformationDAO {
-	
+public interface CreditCardInformationDAO extends GenericDao {
+
 	public static final String BEAN_NAME = "CreditCardInformationDAO";
-	
+
 	public List<CreditCardInformation> findBySupplierManager(Group supplierManager);
 	public CreditCardInformation findByMerchant(String merchantPK, String merchantType);
 	public CreditCardInformation findByPrimaryKey(Integer pk);
 	public void store(CreditCardInformation info);
 	public void delete(CreditCardInformation info);
+
 }
--- src/java/com/idega/block/trade/data/dao/DebitCardInformationDAO.java
+++ src/java/com/idega/block/trade/data/dao/DebitCardInformationDAO.java
@@ -3,9 +3,10 @@ package com.idega.block.trade.data.dao;
 import java.util.List;
 
 import com.idega.block.trade.data.bean.DebitCardInformation;
+import com.idega.core.persistence.GenericDao;
 import com.idega.user.data.bean.Group;
 
-public interface DebitCardInformationDAO {
+public interface DebitCardInformationDAO extends GenericDao {
 
 	public static final String BEAN_NAME = "DebitCardInformationDAO";
 
@@ -15,4 +16,5 @@ public interface DebitCardInformationDAO {
 	public void store(DebitCardInformation info);
 	public void delete(DebitCardInformation info);
 	public List<DebitCardInformation> findBySupplierManager(Integer supplierManager);
+
 }
